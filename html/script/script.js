be784a4deb8e37a648cc1434cbc03bfc99ae06c1
Valentin Senk <senkv@senkvalentin.com>
2021-07-28 15:47:33 +0000

    banner img

--- html/script/script.js
+++
@@ -1,3 +0,0 @@
-document.querySelectorAll('.navbar-collapse ul li a:not(.dropdown-toggle)').forEach((itm) =>{
-    itm.addEventListener('click',() => {document.querySelector('.navbar-toggler').click();});
-});
